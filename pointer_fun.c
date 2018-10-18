9fdd43df6eb4614fbee8098c3a750be0e9f5d015
DanielKastenhofer <daniel.kastenhofer1@gmail.com>
2018-10-18 09:08:53 +0200

    pointer fun

---
+++ pointer_fun.c
@@ -0,0 +1,19 @@
+void print_integers(int x, int* y) {
+  printf("%d %d\n",x, *y);
+}
+int change_integers(int x, int* y) {
+  printf("Bitte geben Sie einen Wert für x ein: \n");
+  scanf("%d", &x);
+  printf("Bitte geben Sie einen wert für y ein: \n");
+  scanf("%d", y);
+
+  return x;
+}
+int main(int argc, char const *argv[]) {
+  int int_value = 666;
+  int* int_pointer = 999;
+  print_integers(int_value, &int_pointer);
+  int_value = change_integers(int_value, &int_pointer);
+  print_integers(int_value, &int_pointer);
+  return 0;
+}
