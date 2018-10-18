9d61f09467e026a19b2b668f1d9db42451cd5897
DanielKastenhofer <daniel.kastenhofer1@gmail.com>
2018-10-18 09:29:50 +0200

    Final version, fixed a few warnings

--- pointer_fun.c
+++ pointer_fun.c
@@ -1,7 +1,10 @@
-void print_integers(int x, int* y) {
+#include <stdio.h>
+
+void print_integers(int x, int *y) {
   printf("%d %d\n",x, *y);
 }
-int change_integers(int x, int* y) {
+
+int change_integers(int x, int *y) {
   printf("Bitte geben Sie einen Wert für x ein: \n");
   scanf("%d", &x);
   printf("Bitte geben Sie einen wert für y ein: \n");
@@ -10,10 +13,12 @@ int change_integers(int x, int* y) {
   return x;
 }
 int main(int argc, char const *argv[]) {
+
+  int value = 999;
   int int_value = 666;
-  int* int_pointer = 999;
-  print_integers(int_value, &int_pointer);
-  int_value = change_integers(int_value, &int_pointer);
-  print_integers(int_value, &int_pointer);
+  int *int_pointer = &value;;
+  print_integers(int_value, int_pointer);
+  int_value = change_integers(int_value, int_pointer);
+  print_integers(int_value, int_pointer);
   return 0;
 }
